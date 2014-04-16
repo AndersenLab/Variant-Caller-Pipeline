3ac165aa6d605938c3d35aa109165384aeea8f92
Dan Cook <dancook@dhcp-129-105-11-251.biochem.northwestern.edu>
2014-04-16 13:48:39 -0500

    removing aln aligning

--- data/temp_cluster_scripts/00_align_paired.sh
+++ data/temp_cluster_scripts/00_align_paired.sh
@@ -36,20 +36,6 @@ BAM_DIR="../bam/"
 f1=${1}_1.fq.gz
 f2=${1}_2.fq.gz
 
-# Unzip the file; Keep original and force
-echo "TIME START aln: $SECONDS"
-for fa in ${f1} ${f2}
-do
-	pwd
-	bwa aln -t 8 ../reference/ce10/ce10.fa $fa > ${fa/.fq.gz/.sai}
-done
-
-bwa sampe ../reference/ce10/ce10.fa ${f1/.fq.gz/.sai} ${f2/.fq.gz/.sai} ${f1} ${f2} | samtools view -b -S -h - | samtools sort - $BAM_DIR`basename ${f1/_1.fq.gz/}`
-# Remove intermediates
-rm ${f1/.fq.gz/.sai}
-rm ${f2/.fq.gz/.sai}
-
-echo "TIME END aln: $SECONDS"
 echo "${BAM_DIR}${f1/.fq.gz/mem}"
 echo "TIME START mem: $SECONDS"
 bwa mem -t 8 ../reference/ce10/ce10.fa ${f1} ${f2} | samtools view -b -S -h  - | samtools sort - ${BAM_DIR}`basename ${f1/.fq.gz/mem}`
