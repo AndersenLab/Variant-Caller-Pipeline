3373b65285303b601706aed027181c97a3083965
Daniel Cook <danielecook@gmail.com>
2014-05-02 16:00:42 -0500

    Added analysis project; added depth stuff

--- scripts/analysis/read_depth.R
+++ scripts/analysis/read_depth.R
@@ -1,10 +1,30 @@
 # Read Depth Stats
 
-bam_depth <- read.table("~/Documents/Spring Rotation/Variant-Caller-Pipeline/data/ancillary/bam_depth.txt", header=T, quote="\"")
+bam_depth <- read.table("../../data/ancillary/bam_depth.txt", header=T, quote="\"",  strip.white = T)
 View(bam_depth)
 
 vars <- c("Run","Library","Strain","junk")
 bam_depth[,vars] <- colsplit(bam_depth$bam, "-|-", vars)
 bam_depth$junk <- NULL
 
-fastq_info <- read.csv("~/Documents/Spring Rotation/Variant-Caller-Pipeline/data/ancillary/fastq_info.csv")
+# Construct bam filename
+fastq_info <- read.csv("../../data/ancillary/fastq_info.txt",  strip.white = T)
+vars <- c("Run","Library","Strain","hash", "ext")
+fastq_info[,vars] <- colsplit(fastq_info$fastq, "-|-", vars)
+
+fastq_info <- group_by(fastq_info,run, library, strain, flowcell, lane) %.%
+  mutate(bam=sprintf("%s-%s-%s-%s.bam", run, library, strain, paste0(unique(hash), collapse='-'))) %.%
+  filter(pair==1) %.%
+  ungroup() %.%
+  select(bam, lane, flowcell)
+
+d <- inner_join(fastq_info, bam_depth, by = 'bam')
+d <- rename(d,c("Library"="lib"))
+
+ggplot(data=d, aes(y=avg_depth,x=lib, order=-desc(lib))) +
+  geom_boxplot() +
+  coord_flip() +
+  facet_grid(Run ~ .) 
+
+
+# Format Fastq info
