a9c975f6674346e57ae90eef1f9c863e938a6458
Daniel Cook <danielecook@gmail.com>
2014-05-08 11:20:06 -0500

    Wrote script for downloading and processing vcf.

--- scripts/prep/02_mmp.sh
+++ scripts/prep/02_mmp.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
 
-cd ../raw/mmp
+# Download mmp data.
+cd ../../data/mmp/
+wget --timestamping 'http://waterston.gs.washington.edu/trackhubs/data/isolate_vcf.tar.gz'
+gunzip isolate_vcf.tar.gz
+tar -xf isolate_vcf.tar 
+rm isolate_vcf.tar
 
-# Download Data from "The million mutation project: A new approach to genetics in Caenorhabditis elegans"
-wget 'http://genome.sfu.ca/mmp/mmp_wild_isolate_data_Mar13.txt'
+# bgzip
+for r in `ls *.vcf`; do
+	bgzip $r
+	tabix $r.gz
+done
+# Combine vcfs
+vcf-merge `ls *.vcf.gz` > mmp.vcf
 
-# Generate individual files for each strain (will be concatenated later)
-mkdir strains
-
-# Generate set of strains
-cut -f 3 mmp_wild_isolate_data_Mar13.txt | sort | uniq > strains.txt
-
-# Generate list of SNPs , filtering out indels & complex changes.
-egrep -v '(insertion|deletion|complex_change)' mmp_wild_isolate_data_Mar13.txt | awk -F $'\t' '{ print "CHROMOSOME_" $4 "\t" $3 "\t" $6 "/" $7 "\t" $5 "\t" $5+1 "\t0\t+\t" 0 "\t"}' |  tail -n +2  > mmp_snps.txt
-
-perl remap_gff_between_releases.pl --release1 235 --release2 220 --out out.test --gff mmp_snps.txt 
-
-
-# Perform liftover
-
-cd ../vcf/
-
-gunzip -kfc ../vcf/04_mmp_strains.txt.Q10.vcf.gz | vcf-to-tab > andersen_mmp_Q10.txt
-
-
-##############
-# Plot stats #
-##############
-
-bcftools stats  -s - 02a_BGI2_rep1.txt.Q40.vcf.gz 02b_BGI2_rep2.txt.Q40.vcf.gz > rep1.txt
-plot-vcfstats -p rep1_rep2/ rep1.txt
+# Remove extra files. Only keep mmp.vcf
+rm `find . -type f -not -name "mmp.vcf"`
