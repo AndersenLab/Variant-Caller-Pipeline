b8231c7dd55c92ec38949a5573f62ffe1509a337
Daniel Cook <danielecook@gmail.com>
2014-05-13 13:26:45 -0500

    Additional fixes to mmp script.

--- scripts/prep/02_mmp.sh
+++ scripts/prep/02_mmp.sh
@@ -7,20 +7,25 @@ gunzip isolate_vcf.tar.gz
 tar -xf isolate_vcf.tar 
 rm isolate_vcf.tar
 
+# Fix ED3017 - appears to be a duplicate line or something.
+sed '107577d' ED3017.wb225.fa.combined.vcf | sed '107576d' ED3017.wb225.fa.combined.vcf > ED3017.f.vcf
+rm ED3017.wb225.fa.combined.vcf
+mv ED3017.f.vcf ED3017.wb225.fa.combined.vcf
+
 # Add ALT genotype column 1/1 to end; 
 for r in `ls *.vcf`; do
     echo $r
     STRAIN_NAME=${r%%\.*}
-    awk -v strain=$STRAIN_NAME '
+    awk -v strain=$STRAIN_NAME -F $'\t' 'BEGIN {OFS = FS}
      $1 ~ "^\##" { print }
-     $1 ~ "^\#CHROM" { print $0 "\t" strain}
-     $1 !~ "^\#" { $3="."; $8="."; print $0 "\t1|1"}
+     $1 ~ "^\#CHROM" { print "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">"; print $0 "\tFORMAT\t" strain}
+     $1 !~ "^\#" { $3="1"; $8 = "."; print $0 "\tGT\t1|1"}
      ' $r > $STRAIN_NAME.fixed.vcf
     bgzip -f $STRAIN_NAME.fixed.vcf
     tabix $STRAIN_NAME.fixed.vcf.gz
 done
 # Combine vcfs; collapse variants.
-vcf-merge -c both `ls *.fixed.vcf.gz` > mmp.vcf
+vcf-merge -R 0/0 -c both `ls *.fixed.vcf.gz` > mmp.vcf
 
 # Compres and Index.
 bgzip mmp.vcf
