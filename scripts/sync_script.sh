9b5af652d55f702577a2b20240266e9fba968a79
Daniel Cook <danielecook@gmail.com>
2014-07-01 16:26:21 -0500

    Cleanup of analysis

--- scripts/sync_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This script will sync cluster / local scripts.
-
-# BE CAREFUL!
-fswatch-run "/Users/dancook/Documents/Spring_Rotation/Variant-Caller-Pipeline/scripts/" "rsync -avz --delete --exclude=.* /Users/dancook/Documents/Spring_Rotation/Variant-Caller-Pipeline/scripts/  dec211@dhunni.biochem.northwestern.edu:/lscr2/andersenlab/dec211/scripts/"
