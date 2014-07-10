f2fca66630ddcebdbaa4bd4db0af824685140e28
Daniel Cook <danielecook@gmail.com>
2014-07-09 19:16:50 -0500

    Cleaning up radseq; now runs independent of files in its folder.

--- scripts/prep/02b_radseq.sh
+++ scripts/prep/02b_radseq.sh
@@ -4,6 +4,8 @@
 
 cd ../../data/radseq/
 
+wget 'https://github.com/AndersenLab/Rad-Seq/blob/master/data/41188SNPset.txt?raw=true' -O 41188SNPset.txt
+
 # Download files necessary to perform liftover.
 wget ftp://ftp.sanger.ac.uk/pub2/wormbase/software/Remap-between-versions/remap.tar.bz2
 gunzip remap.tar.bz2
@@ -14,7 +16,7 @@ for r in `ls Remap-for-other-groups/*`; do
 done;
 
 mv Remap-for-other-groups/* .
-rm -d Remap-between-versions/
+rm -d Remap-for-other-groups/
 
 # Convert to gff
 awk 'NR>1{ split($1,a,"_"); print "CHROMOSOME_" substr($1,0,1) "\t.\t" $1 "\t" a[2] "\t" a[2] "\t.\t+\t.\t."}' 41188SNPset.txt | egrep -v "MtDNA" > 41188SNPset.ws210.gff
@@ -27,7 +29,7 @@ cut -f 3,4,5 41188SNPset.ws210.gff > $ws210
 header=`mktemp -t tmp`
 head -n 1 41188SNPset.txt | awk '{gsub(/^/,"#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t"); print}' > $header
 
-join -t $'\t' $ws210 41188SNPset.txt | gawk -f ../../scripts/prep/02b_radseq_awk.awk | cat vcf_header.txt $header - | bgzip > andersen08_radseq.ws225.vcf.gz
+join -t $'\t' $ws210 41188SNPset.txt | gawk -f ../../scripts/prep/02b_radseq_awk.awk | cat ../ancillary/vcf_header_radseq.txt $header - | bgzip > andersen08_radseq.ws225.vcf.gz
 
 mv andersen08_radseq.ws225.vcf.gz ../vcf/andersen08_radseq.ws225.vcf.gz
 tabix ../vcf/andersen08_radseq.ws225.vcf.gz
