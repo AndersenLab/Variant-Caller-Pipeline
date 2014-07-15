ec19b18ba34ab3a9ed87fe7f6a9ad6031b94144c
Daniel Cook <danielecook@gmail.com>
2014-07-15 00:06:58 -0500

    Radseq Script fixed

--- scripts/prep/02b_radseq.sh
+++ scripts/prep/02b_radseq.sh
@@ -21,7 +21,7 @@ rm -d Remap-for-other-groups/
 # Convert to gff
 awk 'NR>1{ split($1,a,"_"); print "CHROMOSOME_" substr($1,0,1) "\t.\t" $1 "\t" a[2] "\t" a[2] "\t.\t+\t.\t."}' 41188SNPset.txt | egrep -v "MtDNA" > 41188SNPset.ws210.gff
 # liftover
-perl remap_gff_between_releases.pl --release1=210 --release2=225 --gff=41188SNPset.ws210.gff --output=41188SNPset.ws225.gff
+perl remap_gff_between_releases.pl --release1=210 --release2=220 --gff=41188SNPset.ws210.gff --output=41188SNPset.ws220.gff
 
 ws210=`mktemp -t tmp`
 cut -f 3,4,5 41188SNPset.ws210.gff > $ws210
@@ -29,8 +29,8 @@ cut -f 3,4,5 41188SNPset.ws210.gff > $ws210
 header=`mktemp -t tmp`
 head -n 1 41188SNPset.txt | awk '{gsub(/^/,"#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t"); print}' > $header
 
-join -t $'\t' $ws210 41188SNPset.txt | gawk -f ../../scripts/prep/02b_radseq_awk.awk | cat ../ancillary/vcf_header_radseq.txt $header - | bcftools view -O b > andersen08_radseq.ws225.bcf
+join -t $'\t' $ws210 41188SNPset.txt | gawk -f ../../scripts/prep/02b_radseq_awk.awk | cat ../ancillary/vcf_header_radseq.txt $header - | bcftools view -O b > andersen08_radseq.ws220.bcf
 
 
-mv andersen08_radseq.ws225.bcf ../vcf/andersen08_radseq.ws225.bcf
-bcftools index ../vcf/andersen08_radseq.ws225.bcf
+mv andersen08_radseq.ws220.bcf ../vcf/andersen08_radseq.ws220.bcf
+bcftools index ../vcf/andersen08_radseq.ws220.bcf
