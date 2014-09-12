fd3fe3af84d99a53c8d815070eccb394fe7d7c11
Daniel Cook <danielecook@gmail.com>
2014-09-12 18:03:25 -0500

    Preparing Rockman Files

---
+++ scripts/prep/Rockman_prep.sh
@@ -0,0 +1,75 @@
+# Prepare Rockman Files
+# Rename as Appropriate
+mv A6330_Read1.fastq.gz Rockman999-R999.1-A6330-f469d-1.fq.gz
+mv A6330_Read2.fastq.gz Rockman999-R999.1-A6330-984f7-2.fq.gz
+mv AB2_Read1.fastq.gz Rockman999-R999.1-AB2-9d851-1.fq.gz
+mv AB2_Read2.fastq.gz Rockman999-R999.1-AB2-b5a1a-2.fq.gz
+mv CB4856-1_ATCACG_L003_R1_001.fastq.gz Rockman106-R106.3-CB4856-50f7f-1.fq.gz
+mv CB4856-1_ATCACG_L003_R2_001.fastq.gz Rockman106-R106.3-CB4856-15f3f-2.fq.gz
+mv EG4347-2_GATCAG_L004_R1_001.fastq.gz Rockman096-R096.4-EG4347-9bd94-1.fq.gz
+mv EG4347-2_GATCAG_L004_R2_001.fastq.gz Rockman096-R096.4-EG4347-5525f-2.fq.gz
+mv EG4348-2_GGCTAC_L002_R1_001.fastq.gz Rockman096-R096.2-EG4348-125bc-1.fq.gz
+mv EG4348-2_GGCTAC_L002_R2_001.fastq.gz Rockman096-R096.2-EG4348-fd60b-2.fq.gz
+mv JU1088-2_ACTTGA_L004_R1_001.fastq.gz Rockman096-R096.4-JU1088-ed90c-1.fq.gz
+mv JU1088-2_ACTTGA_L004_R2_001.fastq.gz Rockman096-R096.4-JU1088-e0316-2.fq.gz
+mv JU1711-2_TAGCTT_L004_R1_001.fastq.gz Rockman096-R096.4-JU1711-09443-1.fq.gz
+mv JU1711-2_TAGCTT_L004_R2_001.fastq.gz Rockman096-R096.4-JU1711-b05f2-2.fq.gz
+mv MY18-1_CAGATC_L004_R1_001.fastq.gz Rockman096-R096.4-MY18-0b0d7-1.fq.gz
+mv MY18-1_CAGATC_L004_R2_001.fastq.gz Rockman096-R096.4-MY18-f333e-2.fq.gz
+mv QG323_R1.fastq.gz Rockman076-R076.2-QG323-79b92-1.fq.gz
+mv QG323_R2.fastq.gz Rockman076-R076.2-QG323-2a683-2.fq.gz
+mv QG323-3_CAGATC_L002_R1_001.fastq.gz Rockman096-R096.2-QG323-2a19f-1.fq.gz
+mv QG323-3_CAGATC_L002_R2_001.fastq.gz Rockman096-R096.2-QG323-4123c-2.fq.gz
+mv QG324-1_I02CGATGT_CGATGT_L008_R1_001.fastq.gz Rockman083-R083.8-QG324-7141d-1.fq.gz
+mv QG324-1_I02CGATGT_CGATGT_L008_R2_001.fastq.gz Rockman083-R083.8-QG324-f391f-2.fq.gz
+mv QG325_R1.fastq.gz Rockman076-R076.2-QG325-1c75d-1.fq.gz
+mv QG325_R2.fastq.gz Rockman076-R076.2-QG325-f0848-2.fq.gz
+mv QG326-2_ATCACG_L002_R1_001.fastq.gz Rockman096-R096.2-QG326-a7aa6-1.fq.gz
+mv QG326-2_ATCACG_L002_R2_001.fastq.gz Rockman096-R096.2-QG326-fed8d-2.fq.gz
+mv QG327_R1.fastq.gz Rockman076-R076.4-QG327-7420f-1.fq.gz
+mv QG327_R2.fastq.gz Rockman076-R076.4-QG327-22318-2.fq.gz
+mv QG327-3_TTAGGC_L002_R1_001.fastq.gz Rockman096-R096.2-QG327-a317c-1.fq.gz
+mv QG327-3_TTAGGC_L002_R2_001.fastq.gz Rockman096-R096.2-QG327-b28ed-2.fq.gz
+mv QG327-7cyc_R1.fastq.gz Rockman077-R077.7-QG327-590de-1.fq.gz
+mv QG327-7cyc_R2.fastq.gz Rockman077-R077.7-QG327-0a894-2.fq.gz
+mv QG328-1_I05ACAGTG_ACAGTG_L008_R1_001.fastq.gz Rockman083-R083.8-QG328-21ee9-1.fq.gz
+mv QG328-1_I05ACAGTG_ACAGTG_L008_R2_001.fastq.gz Rockman083-R083.8-QG328-3e068-2.fq.gz
+mv QG329_R1.fastq.gz Rockman076-R076.3-QG329-d3895-1.fq.gz
+mv QG329_R2.fastq.gz Rockman076-R076.3-QG329-9241d-2.fq.gz
+mv QG331-2_GCCAAT_L002_R1_001.fastq.gz Rockman096-R096.2-QG331-75676-1.fq.gz
+mv QG331-2_GCCAAT_L002_R2_001.fastq.gz Rockman096-R096.2-QG331-95f7c-2.fq.gz
+mv QG332-2_CGATGT_L002_R1_001.fastq.gz Rockman096-R096.2-QG332-b5ae7-1.fq.gz
+mv QG332-2_CGATGT_L002_R2_001.fastq.gz Rockman096-R096.2-QG332-52d77-2.fq.gz
+mv QG333_R1.fastq.gz Rockman076-R076.3-QG333-db152-1.fq.gz
+mv QG333_R2.fastq.gz Rockman076-R076.3-QG333-49ea7-2.fq.gz
+mv QG334-2_TGACCA_L002_R1_001.fastq.gz Rockman096-R096.2-QG334-6335a-1.fq.gz
+mv QG334-2_TGACCA_L002_R2_001.fastq.gz Rockman096-R096.2-QG334-d03cb-2.fq.gz
+mv QG335_R1.fastq.gz Rockman076-R076.4-QG335-e4d31-1.fq.gz
+mv QG335_R2.fastq.gz Rockman076-R076.4-QG335-9b61e-2.fq.gz
+mv QG335-2_I12CTTGTA_CTTGTA_L008_R1_001.fastq.gz Rockman083-R083.8-QG335-1516c-1.fq.gz
+mv QG335-2_I12CTTGTA_CTTGTA_L008_R2_001.fastq.gz Rockman083-R083.8-QG335-4dba4-2.fq.gz
+mv QG336-2_I07CAGATC_CAGATC_L008_R1_001.fastq.gz Rockman083-R083.8-QG336-e997e-1.fq.gz
+mv QG336-2_I07CAGATC_CAGATC_L008_R2_001.fastq.gz Rockman083-R083.8-QG336-46594-2.fq.gz
+mv QG337-2_ACAGTG_L002_R1_001.fastq.gz Rockman096-R096.2-QG337-d0a5a-1.fq.gz
+mv QG337-2_ACAGTG_L002_R2_001.fastq.gz Rockman096-R096.2-QG337-c6aff-2.fq.gz
+mv QG338-1_I06GCCAAT_GCCAAT_L008_R1_001.fastq.gz Rockman083-R083.8-QG338-b11d8-1.fq.gz
+mv QG338-1_I06GCCAAT_GCCAAT_L008_R2_001.fastq.gz Rockman083-R083.8-QG338-041dd-2.fq.gz
+mv QX1211-1_ACAGTG_L004_R1_001.fastq.gz Rockman106-R106.4-QX1211-28520-1.fq.gz
+mv QX1211-1_ACAGTG_L004_R2_001.fastq.gz Rockman106-R106.4-QX1211-f1ca3-2.fq.gz
+
+
+# Fix up VCF; Chr. Names, etc.
+gunzip WS220.cewi.vcf.tar.gz
+cd WS220.cewi.vcf
+
+for r in  `ls *.stampy*.vcf`; do
+	bgzip $r && tabix $r.gz
+	new_name=`bcftools query -l $r.gz |  sed 's/\/scratch.*stampy\/ce10\///g' | sed 's/\.stampy.ce10.*//g'`
+	bcftools view $r.gz |  sed 's/\/scratch.*stampy\/ce10\///g' | sed 's/\.stampy.ce10.*//g' | sed 's/CHROMOSOME_/chr/g' | bcftools view -O b > $new_name.fixed.bcf
+	bcftools index $new_name.fixed.bcf
+done
+
+bcftools  merge  `ls *fixed.bcf`  | sed "s/.\/.:.:./0\/0:.:./g" | bcftools view -O b > Rockman_ws220.bcf
+bcftools index Rockman_ws220.bcf
+# Set unknowns to 0
+bcftools view Rockman_ws220.bcf  | sed 's/CHROMOSOME_/chr/g' | sed "s/.\/.:.:./0\/0:.:./g" | bcftools view -O b > Rockman_ws220.bcf
