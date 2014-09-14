e0247bc15183c04d7eeddfddcfa1dc8a9917b8cb
Daniel Cook <danielecook@gmail.com>
2014-09-14 12:22:01 -0500

    Cleanup and LCR Stuff

--- scripts/prep/04_LCR_repeat_masker.sh
+++ scripts/prep/04_LCR_repeat_masker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+## LCR 1
 cd ../../data/ancillary/
 wget 'http://hgdownload.soe.ucsc.edu/goldenPath/ce10/database/rmsk.txt.gz' -O LCR_rmsk.txt.gz
 gunzip -kfc LCR_rmsk.txt.gz | grep 'Low_complexity' | cut -f 6,7,8 > LCR_ce10_rmsk.bed
@@ -8,4 +9,11 @@ rm LCR_rmsk.txt.gz
 mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -e \
 	"select chrom, size from ce10.chromInfo"  > ce10.genome
 
-bedtools complement -i LCR_ce10_rmsk.bed  -g ce10.genome | sort -k 1,1 -k2,2n > ce10_no_LCR.bed
+bedtools complement -i LCR_ce10_rmsk.bed  -g ce10.genome | sort -k 1,1 -k2,2n > ce10.ucsc.masked.bed
+
+## LCR 2
+
+wget 'ftp://ftp.wormbase.org/pub/wormbase/releases/WS220/species/c_elegans/c_elegans.WS220.genomic_masked.fa.gz'
+python ../../scripts/misc/generate_masked_ranges.py c_elegans.WS220.genomic_masked.fa.gz CHROMSOME_ chr > WS220.wormbase.masked.bed
+
+## Generate Stats on repeat masking, and store in database.
