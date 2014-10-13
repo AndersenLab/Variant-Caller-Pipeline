40e152d984db90099592b0121ee1be6fec5016fb
Daniel Cook <danielecook@gmail.com>
2014-10-13 11:52:09 -0500

    Switching LCR to ws235

--- scripts/prep/04_LCR_repeat_masker.sh
+++ scripts/prep/04_LCR_repeat_masker.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 ## LCR 1
-cd ../../data/LCR_Region/
-curl 'http://hgdownload.soe.ucsc.edu/goldenPath/ce10/database/rmsk.txt.gz' | gunzip -kfc - | grep 'Low_complexity' | cut -f 6,7,8 | awk '{ print $0 "\tLCR_Region"}' | bedtools merge -i -  > ce10.ucsc.masked.bed
+#cd ../../data/LCR_Region/
+#curl 'http://hgdownload.soe.ucsc.edu/goldenPath/ce10/database/rmsk.txt.gz' | gunzip -kfc - | grep 'Low_complexity' | cut -f 6,7,8 | bedtools merge -i - | awk '{ print $0 "\tLCR_UCSC_ce10_ws220"}'  > ce10.ucsc.masked.bed
 
 # Download c. elegans chromosome information
-mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -e \
-	"select chrom, size from ce10.chromInfo"  > ce10.genome
+#mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -e \
+#	"select chrom, size from ce10.chromInfo"  > ce10.genome
 
 ## LCR 2
 
-curl 'ftp://ftp.wormbase.org/pub/wormbase/releases/WS220/species/c_elegans/c_elegans.WS220.genomic_masked.fa.gz' > c_elegans.WS220.genomic_masked.fa.gz
-python ../../scripts/misc/generate_masked_ranges.py c_elegans.WS220.genomic_masked.fa.gz CHROMOSOME_ chr | sed 's/chrMtDNA/chrM/g' | awk '{ print $0 "\tLCR_Region"}' >  WS220.wormbase.masked.bed
+curl 'ftp://ftp.wormbase.org/pub/wormbase/releases/WS235/species/c_elegans/c_elegans.WS235.genomic_masked.fa.gz' > c_elegans.WS220.genomic_masked.fa.gz
+python ../../scripts/misc/generate_masked_ranges.py c_elegans.WS220.genomic_masked.fa.gz CHROMOSOME_ chr | sed 's/chrMtDNA/chrM/g' | awk '{ print $0 "\tLCR_Wormbase_235"}' >  WS220.wormbase.masked.bed
 
 ## Generate Stats on repeat masking, and store in database.
-bedtools genomecov -i ce10.ucsc.masked.bed -g ce10.genome > ce10.ucsc.masked.genomecov.statsitics.txt
-bedtools genomecov -i WS220.wormbase.masked.bed -g ce10.genome > WS220.wormbase.masked.genomecov.statistics.txt
+#bedtools genomecov -i ce10.ucsc.masked.bed -g ce10.genome > ce10.ucsc.masked.genomecov.statsitics.txt
+bedtools genomecov -i WS235.wormbase.masked.bed -g ce10.genome > WS235.wormbase.masked.genomecov.statistics.txt
 
-bgzip ce10.ucsc.masked.bed
-bgzip WS220.wormbase.masked.bed
-tabix ce10.ucsc.masked.bed.gz
-tabix WS220.wormbase.masked.bed.gz
+#bgzip -f ce10.ucsc.masked.bed
+bgzip -f WS235.wormbase.masked.bed
+#tabix -f ce10.ucsc.masked.bed.gz
+tabix -f WS235.wormbase.masked.bed.gz
 
 #rm c_elegans.WS220.genomic_masked.fa.gz
 #rm LCR_temp.bed
